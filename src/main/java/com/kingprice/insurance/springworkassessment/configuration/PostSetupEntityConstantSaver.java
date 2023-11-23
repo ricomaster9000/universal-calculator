a47db177582a937ffdf5ac47739d3bff9742e1d0
Ricardo <ricorpwd@yahoo.co.za>
2023-11-23 09:20:00 +0200

    Update PostSetupEntityConstantSaver.java

--- src/main/java/com/kingprice/insurance/springworkassessment/configuration/PostSetupEntityConstantSaver.java
+++ src/main/java/com/kingprice/insurance/springworkassessment/configuration/PostSetupEntityConstantSaver.java
@@ -8,6 +8,9 @@ import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
