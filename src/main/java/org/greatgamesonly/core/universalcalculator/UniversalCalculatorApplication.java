38ecd752eec5e83319502570b074dca0a4376fdb
Ricardo <ricorpwd@yahoo.co.za>
2023-12-11 12:44:35 +0200

    Update UniversalCalculatorApplication.java

--- src/main/java/org/greatgamesonly/core/universalcalculator/UniversalCalculatorApplication.java
+++ src/main/java/org/greatgamesonly/core/universalcalculator/UniversalCalculatorApplication.java
@@ -17,7 +17,7 @@ import org.springframework.context.annotation.ComponentScan;
 public class UniversalCalculatorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringWorkAssessmentApplication.class, args);
+		SpringApplication.run(UniversalCalculatorApplication.class, args);
 	}
 
 }
