52eccd2ed9b1dc475e4ec154938fb9001cb8e184
Ricardo Minnaar <Ricardo@27FOURINV.local>
2023-12-12 14:38:34 +0200

    make javadoc description more readable

--- src/main/java/org/greatgamesonly/core/package-info.java
+++ src/main/java/org/greatgamesonly/core/package-info.java
@@ -1,6 +1,6 @@
 /**
- * See also OpenAPI 3 API documentation endpoint: @see <a href="http://universal-calculator.eu-west-1.elasticbeanstalk.com/2y19F09NuVw3vgMvVq1dWx0xQu9-7WVhUJJz0sUrcNMtB6toF5lV3l7m/swagger-ui.html">http://universal-calculator.eu-west-1.elasticbeanstalk.com/swagger-ui.html</a>
- * clone repository (read-only access): git clone https://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@github.com/ricomaster9000/universal-calculator.git
+ * OpenAPI 3 API documentation endpoint: <br>@see <a href="http://universal-calculator.eu-west-1.elasticbeanstalk.com/2y19F09NuVw3vgMvVq1dWx0xQu9-7WVhUJJz0sUrcNMtB6toF5lV3l7m/swagger-ui.html">http://universal-calculator.eu-west-1.elasticbeanstalk.com/swagger-ui.html</a><br><br>
+ * clone repository (read-only access): <br>git clone https://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@github.com/ricomaster9000/universal-calculator.git
  */
 
 package org.greatgamesonly.core;
