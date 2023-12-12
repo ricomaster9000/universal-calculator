561c77b2dd3e8b01d30dab10dfa74cb3513e7c84
Ricardo Minnaar <Ricardo@27FOURINV.local>
2023-12-12 15:01:53 +0200

    updated JAVADOCS_SPECIAL_KEY to be more secure

--- src/main/java/org/greatgamesonly/core/universalcalculator/GlobalConstants.java
+++ src/main/java/org/greatgamesonly/core/universalcalculator/GlobalConstants.java
@@ -6,5 +6,5 @@ public class GlobalConstants {
     public static final int SHORT_DB_TEXT_MAX_CHAR_SIZE = 3000;
     public static final int LONG_DB_TEXT_MAX_CHAR_SIZE = 15000;
 
-    public static final String JAVADOCS_SPECIAL_KEY = "2y19F09NuVw3vgMvVq1dWx0xQu9-7WVhUJJz0sUrcNMtB6toF5lV3l7m";
+    public static final String JAVADOCS_SPECIAL_KEY = "2y19F09NuVw3vgMvVq1dWx0xQu9-7WVhUJJz0sUrcNMtB6toF5lV3l7m-brtf6TPZrySw45nc8rK9kkNg5wca9ZTF-jvMPA7a9c8X9FkQbydk6qUHpsg4Zd2Yy";
 }
