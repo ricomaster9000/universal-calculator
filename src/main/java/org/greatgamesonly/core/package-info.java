cdc8d191c46369b5873da35f9f40c04dd20e9248
Ricardo Minnaar <Ricardo@27FOURINV.local>
2023-12-12 08:50:24 +0200

    added additional changes

--- src/main/java/org/greatgamesonly/core/package-info.java
+++ src/main/java/org/greatgamesonly/core/package-info.java
@@ -1,5 +1,5 @@
 /**
- * See also OpenAPI 3 API documentation endpoint: @see <a href="http://universal-calculator.eu-west-1.elasticbeanstalk.com/swagger-ui.html">http://universal-calculator.eu-west-1.elasticbeanstalk.com/swagger-ui.html</a>
+ * See also OpenAPI 3 API documentation endpoint: @see <a href="http://universal-calculator.eu-west-1.elasticbeanstalk.com/2y19F09NuVw3vgMvVq1dWx0xQu9-7WVhUJJz0sUrcNMtB6toF5lV3l7m/swagger-ui.html">http://universal-calculator.eu-west-1.elasticbeanstalk.com/swagger-ui.html</a>
  */
 
 package org.greatgamesonly.core;
--- src/main/java/org/greatgamesonly/core/universalcalculator/configuration/WebConfig.java
+++ src/main/java/org/greatgamesonly/core/universalcalculator/configuration/WebConfig.java
@@ -8,13 +8,52 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.templatemode.TemplateMode;
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
+import static org.greatgamesonly.core.universalcalculator.GlobalConstants.JAVADOCS_SPECIAL_KEY;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
-        registry.addResourceHandler("/resources/static/**").addResourceLocations("classpath:META-INF/resources/static/");
-        registry.addResourceHandler("/**").addResourceLocations("classpath:META-INF/");
+
+        // css
+        registry.addResourceHandler("/static/"+JAVADOCS_SPECIAL_KEY+"/javadoc/*/**.css").addResourceLocations("classpath:/static/"+JAVADOCS_SPECIAL_KEY+"/javadoc/");
+        registry.addResourceHandler("/resources/static/"+JAVADOCS_SPECIAL_KEY+"/javadoc/*/**.css").addResourceLocations("classpath:META-INF/resources/static/"+JAVADOCS_SPECIAL_KEY+"/javadoc/");
+        registry.addResourceHandler("/"+JAVADOCS_SPECIAL_KEY+"/javadoc/*/**.css").addResourceLocations("classpath:META-INF/"+JAVADOCS_SPECIAL_KEY+"/javadoc/");
+        registry.addResourceHandler("/static/"+JAVADOCS_SPECIAL_KEY+"/javadoc/**.css").addResourceLocations("classpath:/static/"+JAVADOCS_SPECIAL_KEY+"/javadoc/");
+        registry.addResourceHandler("/resources/static/"+JAVADOCS_SPECIAL_KEY+"/javadoc/**.css").addResourceLocations("classpath:META-INF/resources/static/"+JAVADOCS_SPECIAL_KEY+"/javadoc/");
+        registry.addResourceHandler("/"+JAVADOCS_SPECIAL_KEY+"/javadoc/**.css").addResourceLocations("classpath:META-INF/"+JAVADOCS_SPECIAL_KEY+"/javadoc/");
+
+        // js
+        registry.addResourceHandler("/static/"+JAVADOCS_SPECIAL_KEY+"/javadoc/*/**.js").addResourceLocations("classpath:/static/"+JAVADOCS_SPECIAL_KEY+"/javadoc/");
+        registry.addResourceHandler("/resources/static/"+JAVADOCS_SPECIAL_KEY+"/javadoc/*/**.js").addResourceLocations("classpath:META-INF/resources/static/"+JAVADOCS_SPECIAL_KEY+"/javadoc/");
+        registry.addResourceHandler("/"+JAVADOCS_SPECIAL_KEY+"/javadoc/*/**.js").addResourceLocations("classpath:META-INF/"+JAVADOCS_SPECIAL_KEY+"/javadoc/");
+        registry.addResourceHandler("/static/"+JAVADOCS_SPECIAL_KEY+"/javadoc/**.js").addResourceLocations("classpath:/static/"+JAVADOCS_SPECIAL_KEY+"/javadoc/");
+        registry.addResourceHandler("/resources/static/"+JAVADOCS_SPECIAL_KEY+"/javadoc/**.js").addResourceLocations("classpath:META-INF/resources/static/"+JAVADOCS_SPECIAL_KEY+"/javadoc/");
+        registry.addResourceHandler("/"+JAVADOCS_SPECIAL_KEY+"/javadoc/**.js").addResourceLocations("classpath:META-INF/"+JAVADOCS_SPECIAL_KEY+"/javadoc/");
+
+        // png
+        registry.addResourceHandler("/static/"+JAVADOCS_SPECIAL_KEY+"/javadoc/*/**.png").addResourceLocations("classpath:/static/"+JAVADOCS_SPECIAL_KEY+"/javadoc/");
+        registry.addResourceHandler("/resources/static/"+JAVADOCS_SPECIAL_KEY+"/javadoc/*/**.png").addResourceLocations("classpath:META-INF/resources/static/"+JAVADOCS_SPECIAL_KEY+"/javadoc/");
+        registry.addResourceHandler("/"+JAVADOCS_SPECIAL_KEY+"/javadoc/*/**.png").addResourceLocations("classpath:META-INF/"+JAVADOCS_SPECIAL_KEY+"/javadoc/");
+        registry.addResourceHandler("/static/"+JAVADOCS_SPECIAL_KEY+"/javadoc/**.png").addResourceLocations("classpath:/static/"+JAVADOCS_SPECIAL_KEY+"/javadoc/");
+        registry.addResourceHandler("/resources/static/"+JAVADOCS_SPECIAL_KEY+"/javadoc/**.png").addResourceLocations("classpath:META-INF/resources/static/"+JAVADOCS_SPECIAL_KEY+"/javadoc/");
+        registry.addResourceHandler("/"+JAVADOCS_SPECIAL_KEY+"/javadoc/**.png").addResourceLocations("classpath:META-INF/"+JAVADOCS_SPECIAL_KEY+"/javadoc/");
     }
+
+    @Bean
+    public ClassLoaderTemplateResolver secondaryTemplateResolver() {
+        ClassLoaderTemplateResolver secondaryTemplateResolver = new ClassLoaderTemplateResolver();
+        secondaryTemplateResolver.setPrefix("static/"+JAVADOCS_SPECIAL_KEY+"/javadoc/");
+        secondaryTemplateResolver.setSuffix(".html");
+        secondaryTemplateResolver.setTemplateMode(TemplateMode.HTML);
+        secondaryTemplateResolver.setCharacterEncoding("UTF-8");
+        secondaryTemplateResolver.setOrder(1);
+        secondaryTemplateResolver.setCheckExistence(true);
+
+        return secondaryTemplateResolver;
+    }
+
+
+
 }
