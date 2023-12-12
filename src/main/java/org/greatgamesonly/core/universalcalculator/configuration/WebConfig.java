0a0dd45b2e7b7dcb1ff5a90b65d8a6fac0270a2d
Ricardo Minnaar <Ricardo@27FOURINV.local>
2023-12-12 08:16:54 +0200

    applied changes to be able to serve javaDocs via special endpoint

--- src/main/java/org/greatgamesonly/core/universalcalculator/configuration/WebConfig.java
+++ src/main/java/org/greatgamesonly/core/universalcalculator/configuration/WebConfig.java
@@ -1,16 +1,20 @@
 package org.greatgamesonly.core.universalcalculator.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/static/**")
-                .addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/resources/static/**").addResourceLocations("classpath:META-INF/resources/static/");
+        registry.addResourceHandler("/**").addResourceLocations("classpath:META-INF/");
     }
 }
