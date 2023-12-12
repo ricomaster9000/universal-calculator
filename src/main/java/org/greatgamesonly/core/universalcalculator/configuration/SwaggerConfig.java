60487b4830b2297d89774e501b25b34d9faf5647
ricomaster9000 <ricorpwd@yahoo.co.za>
2023-12-12 04:38:05 +0200

    added open api documentation endpoint and javadocs hidden endpoint

--- src/main/java/org/greatgamesonly/core/universalcalculator/configuration/SwaggerConfig.java
+++ src/main/java/org/greatgamesonly/core/universalcalculator/configuration/SwaggerConfig.java
@@ -1,22 +1,17 @@
 package org.greatgamesonly.core.universalcalculator.configuration;
 
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public-apis")
+                .packagesToScan("org.greatgamesonly.core.universalcalculator.controller") // specify the package containing your controllers
+                .pathsToMatch("/api/**") // specify patterns to exclude
                 .build();
     }
 }
