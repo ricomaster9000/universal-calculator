177dc6eab7c33ab282421446ec0b036ce26049ee
Ricardo Minnaar <Ricardo@27FOURINV.local>
2023-12-12 14:53:33 +0200

    remove usage of thymeleaf, added postman collection link to javadoc index page comments

--- src/main/java/org/greatgamesonly/core/package-info.java
+++ src/main/java/org/greatgamesonly/core/package-info.java
@@ -1,6 +1,7 @@
 /**
- * OpenAPI 3 API documentation endpoint: <br>@see <a href="http://universal-calculator.eu-west-1.elasticbeanstalk.com/2y19F09NuVw3vgMvVq1dWx0xQu9-7WVhUJJz0sUrcNMtB6toF5lV3l7m/swagger-ui.html">http://universal-calculator.eu-west-1.elasticbeanstalk.com/swagger-ui.html</a><br><br>
- * clone repository (read-only access): <br>git clone https://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@github.com/ricomaster9000/universal-calculator.git
+ * OpenAPI 3 API documentation endpoint: <br><a href="http://universal-calculator.eu-west-1.elasticbeanstalk.com/2y19F09NuVw3vgMvVq1dWx0xQu9-7WVhUJJz0sUrcNMtB6toF5lV3l7m/swagger-ui.html">http://universal-calculator.eu-west-1.elasticbeanstalk.com/swagger-ui.html</a><br><br>
+ * clone repository (read-only access): <br>git clone https://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@github.com/ricomaster9000/universal-calculator.git <br><br>
+ * postman-collection-link: <a href="https://api.postman.com/collections/2864126-5044291c-23bc-4f04-a587-cf140413415e?access_key=PMAT-01HFXSKV12D8YTPB9QFZ8T2PVM">https://api.postman.com/collections/2864126-5044291c-23bc-4f04-a587-cf140413415e?access_key=PMAT-01HFXSKV12D8YTPB9QFZ8T2PVM</a>
  */
 
 package org.greatgamesonly.core;
