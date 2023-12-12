de004330c4c0072d24cee60bdc9c20d9682304dc
Ricardo Minnaar <Ricardo@27FOURINV.local>
2023-12-12 10:30:05 +0200

    add .html to template name

--- src/main/java/org/greatgamesonly/core/universalcalculator/controller/JavaDocController.java
+++ src/main/java/org/greatgamesonly/core/universalcalculator/controller/JavaDocController.java
@@ -9,7 +9,7 @@ public class JavaDocController {
 
     @GetMapping("static/2y19F09NuVw3vgMvVq1dWx0xQu9-7WVhUJJz0sUrcNMtB6toF5lV3l7m/javadoc/index")
     private String serveJavaDoc() {
-        return "index";
+        return "index.html";
     }
 
 }
