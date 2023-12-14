d88cc35e27437bae64770ece5809b6084122ba08
Ricardo Minnaar <Ricardo@27FOURINV.local>
2023-12-14 10:46:15 +0200

    made use of application.properties file to set some constant values, made GlobalConstants a @service

--- src/main/java/org/greatgamesonly/core/universalcalculator/configuration/WebConfig.java
+++ src/main/java/org/greatgamesonly/core/universalcalculator/configuration/WebConfig.java
@@ -1,29 +1,27 @@
 package org.greatgamesonly.core.universalcalculator.configuration;
 
+import org.greatgamesonly.core.universalcalculator.GlobalConstants;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.ClientHttpRequestFactory;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import static org.greatgamesonly.core.universalcalculator.GlobalConstants.JAVADOCS_SPECIAL_KEY;
-import static org.greatgamesonly.core.universalcalculator.GlobalConstants.NETWORK_REQUEST_TIMEOUT_STANDARD;
-
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Autowired
+    private GlobalConstants globalConstants;
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-
-        registry.addResourceHandler("/static/"+JAVADOCS_SPECIAL_KEY+"/javadoc/*/**").addResourceLocations("classpath:/static/"+JAVADOCS_SPECIAL_KEY+"/javadoc/");
-        registry.addResourceHandler("/resources/static/"+JAVADOCS_SPECIAL_KEY+"/javadoc/*/**").addResourceLocations("classpath:META-INF/resources/static/"+JAVADOCS_SPECIAL_KEY+"/javadoc/");
-        registry.addResourceHandler("/"+JAVADOCS_SPECIAL_KEY+"/javadoc/*/**").addResourceLocations("classpath:META-INF/"+JAVADOCS_SPECIAL_KEY+"/javadoc/");
-        registry.addResourceHandler("/static/"+JAVADOCS_SPECIAL_KEY+"/javadoc/**").addResourceLocations("classpath:/static/"+JAVADOCS_SPECIAL_KEY+"/javadoc/");
-        registry.addResourceHandler("/resources/static/"+JAVADOCS_SPECIAL_KEY+"/javadoc/**").addResourceLocations("classpath:META-INF/resources/static/"+JAVADOCS_SPECIAL_KEY+"/javadoc/");
-        registry.addResourceHandler("/"+JAVADOCS_SPECIAL_KEY+"/javadoc/**").addResourceLocations("classpath:META-INF/"+JAVADOCS_SPECIAL_KEY+"/javadoc/");
+        registry.addResourceHandler("/static/"+globalConstants.JAVADOCS_SPECIAL_KEY()+"/javadoc/*/**").addResourceLocations("classpath:/static/"+globalConstants.JAVADOCS_SPECIAL_KEY()+"/javadoc/");
+        registry.addResourceHandler("/resources/static/"+globalConstants.JAVADOCS_SPECIAL_KEY()+"/javadoc/*/**").addResourceLocations("classpath:META-INF/resources/static/"+globalConstants.JAVADOCS_SPECIAL_KEY()+"/javadoc/");
+        registry.addResourceHandler("/"+globalConstants.JAVADOCS_SPECIAL_KEY()+"/javadoc/*/**").addResourceLocations("classpath:META-INF/"+globalConstants.JAVADOCS_SPECIAL_KEY()+"/javadoc/");
+        registry.addResourceHandler("/static/"+globalConstants.JAVADOCS_SPECIAL_KEY()+"/javadoc/**").addResourceLocations("classpath:/static/"+globalConstants.JAVADOCS_SPECIAL_KEY()+"/javadoc/");
+        registry.addResourceHandler("/resources/static/"+globalConstants.JAVADOCS_SPECIAL_KEY()+"/javadoc/**").addResourceLocations("classpath:META-INF/resources/static/"+globalConstants.JAVADOCS_SPECIAL_KEY()+"/javadoc/");
+        registry.addResourceHandler("/"+globalConstants.JAVADOCS_SPECIAL_KEY()+"/javadoc/**").addResourceLocations("classpath:META-INF/"+globalConstants.JAVADOCS_SPECIAL_KEY()+"/javadoc/");
     }
 
     @Bean
@@ -33,8 +31,8 @@ public class WebConfig implements WebMvcConfigurer {
 
     private SimpleClientHttpRequestFactory clientHttpRequestFactory() {
         SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
-        factory.setReadTimeout(NETWORK_REQUEST_TIMEOUT_STANDARD);
-        factory.setConnectTimeout(NETWORK_REQUEST_TIMEOUT_STANDARD);
+        factory.setReadTimeout(globalConstants.NETWORK_REQUEST_TIMEOUT_STANDARD());
+        factory.setConnectTimeout(globalConstants.NETWORK_REQUEST_TIMEOUT_STANDARD());
         return factory;
     }
 
