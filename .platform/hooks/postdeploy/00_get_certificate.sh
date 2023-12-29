289be18e6681c673499926bd314bf29e0516185a
Ricardo Minnaar <ricorpwd@yahoo.co.za>
2023-12-29 19:24:42 +0200

    install certbot if not present

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
+amazon-linux-extras install epel -y
+yum update -y --skip-broken
+
+# Check if certbot is installed
+if ! yum list installed certbot &>/dev/null; then
+    yum install -y certbot
+    yum install certbot-nginx -y
+fi
+
 sudo certbot -n -d universal-calculator.net --nginx --agree-tos --email ricorpwd@yahoo.co.za --redirect
